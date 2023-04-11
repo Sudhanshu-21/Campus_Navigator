b7d5869bbb433aa74b47c431d45218edb95ad7ab
himanshupatill <94595749+himanshupatill@users.noreply.github.com>
2023-04-12 00:18:15 +0530

    Update final_code.ino
    change in logic and lcd.print

--- final_code.ino
+++ final_code.ino
@@ -6,9 +6,9 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // set up the LCD screen (RS=12, E=11, D4
 
 // Define a struct to hold location data
 struct Location {
-  float lat;
-  float Lon;
-  String name;
+  float lattitude;
+  float longitude;
+  String nameOfPlace;
 };
 
 // Define a lookup table of locations (replace with your own)
@@ -20,81 +20,99 @@ Location locations[] = {
 
 // Define the number of locations in the lookup table
 const int numLocations = sizeof(locations) / sizeof(Location);
-
-void setup() {
-  Serial.begin(9600); // initialize the serial port for debugging
-  gpsSerial.begin(9600); // initialize the software serial port for the GPS module
-  lcd.begin(16, 2); // initialize the LCD screen with 16 columns and 2 rows
-  lcd.print("GPS Lookup Table"); // print a header to the LCD screen
-  delay(2000);
+int startsWith(const char* str, const char* prefix) {
+  while (*prefix) {
+    if (*prefix++ != *str++) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
-int indexOf(char* str, char* substr) {
+int indexOf(char* str, int start, char* substr) {
     int len1 = strlen(str);
     int len2 = strlen(substr);
     int i, j;
 
-    for (i = 0; i <= len1 - len2; i++) {
+    for (i = start; i <= len1 - len2; i++) {
         for (j = 0; j < len2; j++) {
             if (str[i+j] != substr[j]) {
                 break;
             }
         }
         if (j == len2) {
-            return i;
+            return i-len2;
         }
     }
 
     return -1;
 }
 
+void setup() {
+  Serial.begin(9600); // initialize the serial port for debugging
+  gpsSerial.begin(9600); // initialize the software serial port for the GPS module
+  lcd.begin(16, 2); // initialize the LCD screen with 16 columns and 2 rows
+  lcd.print("GPS Lookup Table"); // print a header to the LCD screen
+  delay(100);
+  lcd.clear();
+}
 
 void loop() {
-      String locationName = locations[2].name;
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("Lat: ");
-      delay(1000);
-      //lcd.print(Location.lat, 6);
-      delay(10);
-      lcd.setCursor(0, 1);
-      lcd.print("Lon: ");
-      delay(1000);
-      //lcd.print(Location.lon, 6);
-      delay(1000);
-      //lcd.setCursor(0, 1);
-      lcd.print(locationName);
-      delay(10);
   // Read data from the GPS module
-  String gpsData = gpsSerial.readStringUntil('\n');
-  if (gpsData.startsWith("$GPGGA")) { // only process GGA messages
-    // Parse the latitude and longitude from the GPS data
-    int comma1 = gpsData.indexOf(",");
-    int comma2 = gpsData.indexOf(",", comma1 + 1);
-    int comma3 = gpsData.indexOf(",", comma2 + 1);
-    int comma4 = gpsData.indexOf(",", comma3 + 1);
-    int comma5 = gpsData.indexOf(",", comma4 + 1);
-    float lattitude = gpsData.substring(comma1 + 1, comma2).toFloat();
-    float longitude = gpsData.substring(comma3 + 1, comma4).toFloat();
+    float lattitude=0;
+    float longitude=0;
+    String gpsData = gpsSerial.readStringUntil('\n');
+    
+    lcd.print("check0: ");
+    delay(1000);
+    lcd.clear();
+    if (gpsData.startsWith("$GPGGA")) { // only process GGA messages
+      // Parse the latitude and longitude from the GPS data
+      lcd.setCursor(0, 1);
+      lcd.print("check1: ");
+      delay(1000);
+      char* ptr = strcpy(new char[gpsData.length() + 1], gpsData.c_str());
+      int comma1 = indexOf(ptr, ",", 0);
+      int comma2 = indexOf(ptr, ",", comma1 + 1);
+      int comma3 = indexOf(ptr, ",", comma2 + 1);
+      int comma4 = indexOf(ptr, ",", comma3 + 1);
+      int comma5 = indexOf(ptr, ",", comma4 + 1);
+      float lattitude = gpsData.substring(comma1 + 1, comma2).toFloat();
+      float longitude = gpsData.substring(comma3 + 1, comma4).toFloat();
+    }
     // Look up the name of the location from the latitude and longitude
-
-//    for (int i = 0; i < numLocations; i++) {
-//      if (abs(locations[i].lat - lat) < 10 && abs(locations[i].lon - lon) < 10) { // allow for some rounding error
-//        locationName = locations[i].name;
-//        break;
-//      }
-//    }
+    String locationName = "Location not found";
+    for (int i = 0; i < numLocations; i++) {
+      if (abs(locations[i].lattitude - lattitude) < 10 && abs(locations[i].longitude - longitude) < 10) { // allow for some rounding error
+        locationName = locations[i].nameOfPlace;
+        break;
+      }
+    }
     
     // Print the location name to the LCD screen
-
+      lcd.clear();
+      lcd.print("Lat: ");
+      delay(1000);
+      lcd.setCursor(0, 1);
+      lcd.print(lattitude);
+      delay(1000);
+      lcd.clear();
+      lcd.print("Lon: ");
+      delay(1000);
+      lcd.setCursor(0, 1);
+      lcd.print(longitude);
+      delay(1000);
+      lcd.clear();
+      lcd.print(locationName);
+      delay(10000);
     
   
     // output
     Serial.print("Lat: ");
-    //Serial.print(lat, 6);
+    Serial.print(lattitude, 6);
     Serial.print(", Lon: ");
-   // Serial.print(lon, 6);
+    Serial.print(longitude, 6);
     Serial.print(", Location: ");
     Serial.println(locationName);
-  }
+  
 }
